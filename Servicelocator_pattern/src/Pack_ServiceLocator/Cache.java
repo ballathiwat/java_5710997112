94155c0f220b106074133d0a9cdf5a4652ecf8cf
ballathiwat <athiwat2555@gmail.com>
2017-02-10 22:21:36 +0700

    Lab4 : Map Design Pattern to Soure code [5 Project]

---
+++ Servicelocator_pattern/src/Pack_ServiceLocator/Cache.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Pack_ServiceLocator;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author SCM
+ */
+public class Cache {
+    
+    private List<Service> service;
+    
+    public Cache(){};
+    public void getService(){};
+    public void addService(){};
+}
