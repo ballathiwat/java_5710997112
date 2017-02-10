fc6d07e1be3a53acdd67c55d1b4b08665b32c77d
ballathiwat <athiwat2555@gmail.com>
2017-02-10 18:48:43 +0700

    Lab4 : Map Design Pattern to Soure code [5 Project]

---
+++ Proxy_pattern/src/Pack_proxy/ProxyImage.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pack_proxy;
+
+/**
+ *
+ * @author SCM
+ */
+public class ProxyImage implements Image {
+    
+    public RealImage realImag;
+    public String fileName;
+    
+    
+    
+    
+    @Override
+    public void display(){};
+    public ProxyImage(){};
+    
+}
